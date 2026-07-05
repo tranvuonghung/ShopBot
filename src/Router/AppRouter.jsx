42db7632dc5a8714f1a7eed8625d0aaab356cbb2
nguyentrongduong <nguyentrongduong205@gmail.com>
2026-07-05 18:01:29 +0700

    fix lỗi import

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import Dashboard from "../Page/Dashboard/Dashboard";
-import FoodManagement from "../Page/Menu/Menu";
-import OrdersDashboardPage from "../Page/Order/Order";
-
+import Dashboard from "../Page/Admin/Dashboard/Dashboard";
+import FoodManagement from "../Page/Admin/Menu/Menu";
+import OrdersDashboardPage from "../Page/Admin/Order/Order";
+import PaymentPage from "../client/payment/payment";
+import HistoryPage from "../Page/Admin/history/history";
+import SettingPage from "../Page/Admin/Setting/setting";
+import LoginAdmin from "../Page/Admin/Login/LoginAdmin";
+import ForgotPassword from "../Page/Admin/ForgotPassword/ForgotPassword";
+import UserMenu from "../Page/User/Menu/UserMenu";
+import ShoppingCart from "../Page/User/Shopping_Cart/ShoppingCart";
 
 export const router = createBrowserRouter([
   // Admin
